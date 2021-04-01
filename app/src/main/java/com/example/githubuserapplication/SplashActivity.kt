aa2e83a2f6035280316a6ca17ac044fdca421949
Dani Ihza Farrosi <dani.videda@gmail.com>
2021-04-01 22:52:03 +0700

    Implement splash screen, change app logo

---
+++ app/src/main/java/com/example/githubuserapplication/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.example.githubuserapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.concurrent.schedule
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val moveIntent = Intent(this, MainActivity::class.java)
+        Timer().schedule(500) {
+            startActivity(moveIntent)
+            finish()
+        }
+    }
+}
